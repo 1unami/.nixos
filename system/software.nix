7662804e73ffe3332b018483636af20f3d6d8ee8
1unami <135032514+1unami@users.noreply.github.com>
2023-06-01 22:01:18 +0200

    plasma + update

--- system/software.nix
+++ system/software.nix
@@ -1,4 +1,19 @@
 { pkgs, ... }: {
+  services.xserver = {
+    enable = true;
+
+    excludePackages = [
+      pkgs.xterm
+    ];
+
+    desktopManager.plasma5.enable = true;
+    displayManager.sddm.enable = true;
+  };
+
+  environment.plasma5.excludePackages = [
+    pkgs.libsForQt5.elisa
+  ];
+
   environment.systemPackages = with pkgs; [
     git
   ];
